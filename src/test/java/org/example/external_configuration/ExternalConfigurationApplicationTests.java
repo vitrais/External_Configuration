12879cd8e716f19d6c96449cffd12983ea5aff8b
Fomin Viktor <vitraispn@yandex.ru>
2025-10-21 03:23:12 +0300

    error

---
+++ src/test/java/org/example/external_configuration/ExternalConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.external_configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
